547ea90a46d5145fe4a75ea4c360191a24497717
Ray Ahmed <rayanahmed0108@gmail.com>
2026-04-03 16:14:03 -0400

    Added a "player" script

---
+++ Assets/_Project/Scripts/Runtime/SpaceShip.cs
@@ -0,0 +1,34 @@
+using UnityEngine;
+
+public class SpaceShip : MonoBehaviour
+{
+
+    float health;
+    int ammo;
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        health = 100f;
+        ammo = 100;
+    }
+
+    public void TakeDamage(float damage)
+    {
+        health -= damage;
+        
+    }
+
+    // TODO: Add a function that will make the player lose if health is 0 or less.
+
+    public void UseAmmo(int amount)
+    {
+        ammo -= amount;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
